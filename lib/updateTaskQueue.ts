c13bbc5036f785dc128038b08c0d53109c8e6e27
Krzysztof Tomaszewski <drteski@gmail.com>
2025-06-16 23:45:17 +0200

    init

--- lib/updateTaskQueue.ts
+++ lib/updateTaskQueue.ts
@@ -1,75 +1,71 @@
-import payload, { CollectionSlug } from 'payload';
-import payloadConfig from '@/payload.config';
+import payload, { CollectionSlug } from 'payload'
+import payloadConfig from '@/payload.config'
 
 export const updateTaskQueue = async ({
-	                                      action,
-	                                      collection,
-	                                      taskName,
-	                                      start,
-	                                      finish,
-	                                      status
-                                      }: {
-	action: string
-	collection: CollectionSlug
-	taskName: string
-	start: boolean
-	finish: boolean
-	status: string
+  action,
+  collection,
+  taskName,
+  start,
+  finish,
+  status,
+}: {
+  action: string
+  collection: CollectionSlug
+  taskName: string
+  start: boolean
+  finish: boolean
+  status: string
 }) => {
-	return new Promise((resolve, reject) => {
-		;(async () => {
-			try {
-				await payload.init({ config: payloadConfig });
-				if (action === 'create') {
-					const createTask = await payload.create({
-						collection,
-						data: {
-							name: taskName
-						}
-					});
-					return resolve(createTask);
-				}
-				if (action === 'delete') {
-					const createTask = await payload.delete({
-						collection,
-						where: {
-							name: {
-								equals: taskName
-							}
-						}
-					});
-					return resolve(createTask);
-				}
-				if (action === 'update') {
-					const task = await payload.find({
-						collection,
-						where: {
-							name: {
-								equals: taskName
-							}
-						}
-					});
+  try {
+    await payload.init({ config: payloadConfig })
+    if (action === 'create') {
+      const createTask = await payload.create({
+        collection,
+        data: {
+          name: taskName,
+        },
+      })
+      return
+    }
+    if (action === 'delete') {
+      const createTask = await payload.delete({
+        collection,
+        where: {
+          name: {
+            equals: taskName,
+          },
+        },
+      })
+      return
+    }
+    if (action === 'update') {
+      const task = await payload.find({
+        collection,
+        where: {
+          name: {
+            equals: taskName,
+          },
+        },
+      })
 
-					if (task.docs.length === 0) return resolve(null);
+      if (task.docs.length === 0) return
 
-					const updatedTask = await payload.update({
-						collection,
-						id: task.docs[0].id,
-						data: {
-							logs: JSON.stringify({
-								status
-							}),
-							status,
-							startedAt: start ? `${new Date()}` : task.docs[0].startedAt,
-							finishedAt: finish ? `${new Date()}` : null
-						}
-					});
+      const updatedTask = await payload.update({
+        collection,
+        id: task.docs[0].id,
+        data: {
+          logs: JSON.stringify({
+            status,
+          }),
+          status,
+          startedAt: start ? `${new Date()}` : task.docs[0].startedAt,
+          finishedAt: finish ? `${new Date()}` : null,
+        },
+      })
 
-					return resolve(updatedTask);
-				}
-			} catch (error) {
-				reject(error);
-			}
-		})();
-	});
-};
+      return
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
